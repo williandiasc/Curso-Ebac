6d17912a9eac9db8a734df35e7bef768133f35ba
Willian Dias <wtdiasc@gmail.com>
2023-09-09 18:13:51 -0300

    Exercicio EBAC

---
+++ PrimeiroTeste/src/br/com/wdias/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.wdias;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello World");
+	}
+
+}
